1d2f1ccf95d865302127716e9c745f05276955cd
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2014-12-11 05:53:30 +0100

    add translation of umlaute

---
+++ R/replace_umlaute.r
@@ -0,0 +1,23 @@
+example.replace.german.umlaute = function()
+{
+  setwd("D:/libraries/RTutor/examples")
+  file = "Blatt4_sol.rmd"
+  txt = replace.german.umlaute(file=file)
+}
+
+#' replaces German Umlaute like ö,ü,ä with ascii letters oe, ue, ae
+replace.german.umlaute = function(txt=readLines(file), file=NULL, write.file = !is.null(file)) {
+  txt = gsub("?","ae",txt, fixed=TRUE)
+  txt = gsub("?","oe",txt, fixed=TRUE)
+  txt = gsub("?","ue",txt, fixed=TRUE)
+  txt = gsub("?","ss",txt, fixed=TRUE)
+
+  txt = gsub("?","Ae",txt, fixed=TRUE)
+  txt = gsub("?","Oe",txt, fixed=TRUE)
+  txt = gsub("?","Ue",txt, fixed=TRUE)
+  
+  if (write.file) {
+    writeLines(txt,file)
+  }
+  invisible(txt)
+}
